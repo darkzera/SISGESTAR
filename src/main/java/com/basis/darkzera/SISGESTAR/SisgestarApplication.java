42c19c99a688c4bc364ef0b98b9a727877caa597
darkzera <lucas.zrc@gmail.com>
2021-09-28 13:50:41 -0300

    Initializ

---
+++ src/main/java/com/basis/darkzera/SISGESTAR/SisgestarApplication.java
@@ -0,0 +1,13 @@
+package com.basis.darkzera.SISGESTAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgestarApplication.class, args);
+	}
+
+}
